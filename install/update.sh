5502a05673dbb24ff98529a33da1e335115cb440
Manuel Reinhard <manu@sprain.ch>
2015-12-16 12:53:54 +0100

    Improved printing setup

--- install/update.sh
+++ install/update.sh
@@ -8,6 +8,15 @@ composer install
 php app/console doctrine:schema:update --force --env=prod
 php app/console cache:clear --env=prod
 
+# REPLACE CONF SCRIPTS
+sudo cp /ticketpark/printbox/install/etc/cups/cupsd.conf /etc/cups/cupsd.conf
+sudo /etc/init.d/cups restart
+
+sudo cp /ticketpark/printbox/install/etc/network/interfaces /etc/network/interfaces
+sudo cp /ticketpark/printbox/install/etc/default/isc-dhcp-server /etc/default/isc-dhcp-server
+sudo cp /ticketpark/printbox/install/etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf
+sudo cp /ticketpark/printbox/install/etc/rc.local /etc/rc.local
+
 # PREPARE NEW UPDATE SCRIPT
 sudo cp /ticketpark/printbox/install/update.sh /ticketpark/update.sh
 
